83c83072e0ee796a0a8e35838c6780d458b3e988
Joshua Peek <josh@users.noreply.github.com>
2025-12-16 17:30:00 -0800

    Make tsnet optional

--- main.go
+++ main.go
@@ -17,20 +17,29 @@ import (
 	"tailscale.com/tsnet"
 )
 
+const (
+	defaultMQTTHostname = "mqtt2nats-mqtt"
+	defaultNATSHostname = "mqtt2nats-nats"
+)
+
 func main() {
+	var mqttTsnetEnabled bool
 	var mqttBroker string
 	var mqttTsAuthKey string
 	var mqttTsHostname string
+	var natsTsnetEnabled bool
 	var natsTsAuthKey string
 	var natsTsHostname string
 	var natsURL string
 
 	flag.StringVar(&mqttBroker, "mqtt", "", "MQTT Broker URL (required)")
+	flag.BoolVar(&mqttTsnetEnabled, "mqtt-tsnet", false, "Use tsnet for MQTT connection")
 	flag.StringVar(&mqttTsAuthKey, "mqtt-ts-authkey", "", "Tailscale auth key for MQTT connection")
-	flag.StringVar(&mqttTsHostname, "mqtt-hostname", "mqtt2nats-mqtt", "Tailscale hostname for MQTT connection")
-	flag.StringVar(&natsTsAuthKey, "nats-ts-authkey", "", "Tailscale auth key for NATS connection")
-	flag.StringVar(&natsTsHostname, "nats-hostname", "mqtt2nats-nats", "Tailscale hostname for NATS connection")
+	flag.StringVar(&mqttTsHostname, "mqtt-ts-hostname", defaultMQTTHostname, "Tailscale hostname for MQTT connection")
 	flag.StringVar(&natsURL, "nats", "", "NATS Server URL (required)")
+	flag.BoolVar(&natsTsnetEnabled, "nats-tsnet", false, "Use tsnet for NATS connection")
+	flag.StringVar(&natsTsAuthKey, "nats-ts-authkey", "", "Tailscale auth key for NATS connection")
+	flag.StringVar(&natsTsHostname, "nats-ts-hostname", defaultNATSHostname, "Tailscale hostname for NATS connection")
 	flag.Parse()
 
 	if mqttBroker == "" || natsURL == "" {
@@ -38,41 +47,48 @@ func main() {
 		os.Exit(1)
 	}
 
-	stateDir := os.Getenv("XDG_STATE_HOME")
-	if stateDir == "" {
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			slog.Error("Error getting user home dir", "error", err)
+	if !mqttTsnetEnabled && (mqttTsAuthKey != "" || mqttTsHostname != defaultMQTTHostname) {
+		slog.Error("MQTT tsnet flags provided without -mqtt-tsnet enabled")
+		os.Exit(1)
+	}
+
+	if !natsTsnetEnabled && (natsTsAuthKey != "" || natsTsHostname != defaultNATSHostname) {
+		slog.Error("NATS tsnet flags provided without -nats-tsnet enabled")
+		os.Exit(1)
+	}
+
+	var stateDir string
+	if mqttTsnetEnabled || natsTsnetEnabled {
+		stateDir = os.Getenv("XDG_STATE_HOME")
+		if stateDir == "" {
+			homeDir, err := os.UserHomeDir()
+			if err != nil {
+				slog.Error("Error getting user home dir", "error", err)
+				os.Exit(1)
+			}
+			stateDir = filepath.Join(homeDir, ".local", "state")
+		}
+	}
+
+	var natsOpts []nats.Option
+	var natsTs *tsnet.Server
+
+	if natsTsnetEnabled {
+		natsTs = &tsnet.Server{
+			Hostname: natsTsHostname,
+			AuthKey:  natsTsAuthKey,
+			Dir:      filepath.Join(stateDir, "mqtt2nats", "nats"),
+		}
+		defer natsTs.Close()
+
+		if err := natsTs.Start(); err != nil {
+			slog.Error("Error starting NATS tsnet server", "error", err)
 			os.Exit(1)
 		}
-		stateDir = filepath.Join(homeDir, ".local", "state")
+
+		natsOpts = append(natsOpts, nats.SetCustomDialer(&TailscaleDialer{srv: natsTs}))
 	}
 
-	natsTs := new(tsnet.Server)
-	natsTs.Hostname = natsTsHostname
-	natsTs.AuthKey = natsTsAuthKey
-	natsTs.Dir = filepath.Join(stateDir, "mqtt2nats", "nats")
-	defer natsTs.Close()
-
-	if err := natsTs.Start(); err != nil {
-		slog.Error("Error starting NATS tsnet server", "error", err)
-		os.Exit(1)
-	}
-
-	mqttTs := new(tsnet.Server)
-	mqttTs.Hostname = mqttTsHostname
-	mqttTs.AuthKey = mqttTsAuthKey
-	mqttTs.Dir = filepath.Join(stateDir, "mqtt2nats", "mqtt")
-	defer mqttTs.Close()
-
-	if err := mqttTs.Start(); err != nil {
-		slog.Error("Error starting MQTT tsnet server", "error", err)
-		os.Exit(1)
-	}
-
-	natsOpts := []nats.Option{
-		nats.SetCustomDialer(&TailscaleDialer{srv: natsTs}),
-	}
 	nc, err := nats.Connect(natsURL, natsOpts...)
 	if err != nil {
 		slog.Error("Error connecting to NATS", "error", err)
@@ -84,9 +100,25 @@ func main() {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(mqttBroker)
 	opts.SetClientID("mqtt2nats-relay")
-	opts.SetCustomOpenConnectionFn(func(uri *url.URL, options mqtt.ClientOptions) (net.Conn, error) {
-		return mqttTs.Dial(context.Background(), "tcp", uri.Host)
-	})
+
+	if mqttTsnetEnabled {
+		mqttTs := &tsnet.Server{
+			Hostname: mqttTsHostname,
+			AuthKey:  mqttTsAuthKey,
+			Dir:      filepath.Join(stateDir, "mqtt2nats", "mqtt"),
+		}
+		defer mqttTs.Close()
+
+		if err := mqttTs.Start(); err != nil {
+			slog.Error("Error starting MQTT tsnet server", "error", err)
+			os.Exit(1)
+		}
+
+		opts.SetCustomOpenConnectionFn(func(uri *url.URL, options mqtt.ClientOptions) (net.Conn, error) {
+			return mqttTs.Dial(context.Background(), "tcp", uri.Host)
+		})
+	}
+
 	opts.SetOnConnectHandler(func(client mqtt.Client) {
 		slog.Info("Connected to MQTT", "broker", mqttBroker)
 	})
